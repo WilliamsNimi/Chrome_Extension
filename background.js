281a3757ec49d56136ae3e20bb7eb2044ea3ab37
Nimi Williams <williamson.nimi@gmail.com>
2024-10-09 13:59:56 +0100

    Opted to save to bookmarks instead

---
+++ background.js
@@ -0,0 +1,34 @@
+let tabs = [];
+
+chrome.tabs.query({}, (tab) => {
+        tabs = tab;
+    });
+
+
+async function tab_manager(){
+
+    let tabURLs = [];
+    let today = new Date();
+    
+    for (let i = 0; (i < 1); i++){
+        chrome.bookmarks.create({
+            title: tabs[i].title.split()[0] + today.toString(),
+            url: tabs[i].url
+        });
+        await chrome.tabs.remove(tabs[i].id);
+    }
+    await chrome.tabs.query({}, (tab) => {
+        tabs = tab;
+    });
+
+}
+
+  chrome.alarms.onAlarm.addListener((alarm) => {
+    if (alarm.name === 'myAlarm') {
+      tab_manager();
+    }
+  });
+
+  
+
+  
--- popup.js
+++ popup.js
@@ -1,44 +1,30 @@
-const tabs = await chrome.tabs.query({});
-
-function tab_manager(){
-    let tabURLs = [];
-    let files = document.createElement('a');
-    let bfiles = new Blob([""], {type: 'text/plain'});
-
-    for (let i = 0; i < tabs.length; i++){
-        tabURLs.push([tabs[i].url,tabs[i].title]);
-        chrome.tabs.remove(tabs[i].id)
-    }
-    bfiles = new Blob([tabURLs.toString()], {type: 'text/plain'});
-    files.href = URL.createObjectURL(bfiles);
-    files.download = "savedTabs.txt";
-    files.click();
-    URL.revokeObjectURL(bfiles);
-}
-
-
+let chosen_time_frame;
 const button = document.querySelector("button");
 button.addEventListener("click", async () => {
-    let startDate = new Date()
-    let endDate = new Date()
-    endDate = new Date(endDate.setDate(startDate.getDate() + getOption()))
+    //document.getElementById('#alert').innerHTML = "Your open Chrome tabs will be saved to your bookmarks and cleared. This will occur based on the timeframe you have set";
+    chosen_time_frame = getOption();
+    console.log(chosen_time_frame);
 
-    tab_manager();
+    chrome.alarms.create('myAlarm', {
+        delayInMinutes: chosen_time_frame, // Set the delay to chosen time frame
+        periodInMinutes: chosen_time_frame // Set the period to chosen time frame
+      });
     
   });
-  
+
 
 function getOption() {
     let selectElement = document.querySelector('#select_tag');
     let time_frame = selectElement.options[selectElement.selectedIndex].value;
     let time_frame_int = 0;
-    if (time_frame == "Weekly")
-        time_frame_int = 7
+    if (time_frame == "30Mins")
+        time_frame_int = 30;
+    else if (time_frame == "Weekly")
+        time_frame_int = 7*24*60;
     else if (time_frame == "Bi_Monthly")
-        time_frame_int = 14
+        time_frame_int = 14*24*60
     else
-        time_frame_int = 30
+        time_frame_int = 30*24*60;
 
     return time_frame_int;
 }
-
